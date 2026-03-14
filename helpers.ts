0f99f40345c006d810648f2c023adfe99c13e772
brkreis2 <brkres@outlook.com.tr>
2026-03-15 00:14:29 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:14:13.899Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-14T21:14:29.335Z
+Commit: Correct typo in configuration file
