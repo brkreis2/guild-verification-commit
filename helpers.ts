04e5b5904e7884f4442abec6642fcd62396c9f5f
brkreis2 <brkres@outlook.com.tr>
2026-03-15 00:14:37 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:14:29.335Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-14T21:14:36.711Z
+Commit: Add export functionality for reports
