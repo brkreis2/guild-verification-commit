c431ba3bdcf791e23d195af0cf92a43101c97555
brkreis2 <brkres@outlook.com.tr>
2026-03-15 00:13:48 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:13:45.837Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T21:13:48.005Z
+Commit: Fix broken links in documentation
