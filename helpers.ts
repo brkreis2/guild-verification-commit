3bf0adfab0d799769feb566ffa0dd4961eb92343
brkreis2 <brkres@outlook.com.tr>
2026-03-15 00:15:23 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:15:00.400Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T21:15:23.353Z
+Commit: Fix broken links in documentation
