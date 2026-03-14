e2f39ca3083358696e4aaea0e8c4cb875f7d1e52
brkreis2 <brkres@outlook.com.tr>
2026-03-15 00:13:46 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:18:57.116Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T21:13:45.837Z
+Commit: Patch API endpoint error handling
