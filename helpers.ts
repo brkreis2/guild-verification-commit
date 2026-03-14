c9726da4479acf830a77ac349a4ef4612146be73
brkreis2 <brkres@outlook.com.tr>
2026-03-14 23:18:56 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:18:48.606Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T20:18:55.949Z
+Commit: Resolve null pointer exception in user service
